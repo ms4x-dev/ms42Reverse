e2ba7f7b6a8bb340984d370af9963fd669e36b46
Richard <richard@dsp.net.au>
2025-08-09 11:01:09 +1000

    Initial Commit

---
+++ ms42Reverse/ms42ReverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ms42ReverseApp.swift
+//  ms42Reverse
+//
+//  Created by Richard on 9/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct ms42ReverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ms42ReverseTests/ms42ReverseTests.swift
@@ -0,0 +1,17 @@
+//
+//  ms42ReverseTests.swift
+//  ms42ReverseTests
+//
+//  Created by Richard on 9/8/2025.
+//
+
+import Testing
+@testable import ms42Reverse
+
+struct ms42ReverseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
